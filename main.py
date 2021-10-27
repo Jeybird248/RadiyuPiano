6429f2cd4b618b82a583ac6e6997362871d221e5
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2021-10-27 14:53:31 -0700

    fixed image location errors

--- main.py
+++ main.py
@@ -1,124 +1,170 @@
 # 1 - Import library
 import pygame
-from pygame.locals import *
 from multiprocessing import Process
 
 # 2 - Initialize the game and import graphics
 pygame.init()
-window = pygame.display.set_mode((600, 600))
+window = pygame.display.set_mode((1280, 720))
 
-A = pygame.mixer.sound.load("audio/a.wav")
-B = pygame.mixer.sound.load("audio/b.wav")
-C = pygame.mixer.sound.load("audio/c.wav")
-D = pygame.mixer.sound.load("audio/d.wav")
-E = pygame.mixer.sound.load("audio/e.wav")
-F = pygame.mixer.sound.load("audio/f.wav")
-G = pygame.mixer.sound.load("audio/g.wav")
+lowC = pygame.mixer.Sound("audio/1.wav")
+lowD = pygame.mixer.Sound("audio/2.wav")
+E = pygame.mixer.Sound("audio/3.wav")
+F = pygame.mixer.Sound("audio/4.wav")
+G = pygame.mixer.Sound("audio/5.wav")
+A = pygame.mixer.Sound("audio/6.wav")
+B = pygame.mixer.Sound("audio/7.wav")
+highC = pygame.mixer.Sound("audio/8.wav")
+highD = pygame.mixer.Sound("audio/9.wav")
+Special = pygame.mixer.Sound("audio/0.wav")
 
-#Import graphics
-radiyuA = [pygame.image.load("images/a1.png"), pygame.image.load("images/a2.png"), pygame.image.load("images/a3.png")]
-radiyuB = [pygame.image.load("images/b1.png"), pygame.image.load("images/b2.png"), pygame.image.load("images/b3.png")]
-radiyuC = [pygame.image.load("images/c1.png"), pygame.image.load("images/c2.png"), pygame.image.load("images/c3.png")]
-radiyuD = [pygame.image.load("images/d1.png"), pygame.image.load("images/d2.png"), pygame.image.load("images/d3.png")]
-radiyuE = [pygame.image.load("images/e1.png"), pygame.image.load("images/e2.png"), pygame.image.load("images/e3.png")]
-radiyuF = [pygame.image.load("images/f1.png"), pygame.image.load("images/f2.png"), pygame.image.load("images/f3.png")]
-radiyuG = [pygame.image.load("images/g1.png"), pygame.image.load("images/g2.png"), pygame.image.load("images/g3.png")]
-window.blit(radiyuA[0], (x, y))
-window.blit(radiyuB[0], (x, y))
-window.blit(radiyuC[0], (x, y))
-window.blit(radiyuD[0], (x, y))
-window.blit(radiyuE[0], (x, y))
-window.blit(radiyuF[0], (x, y))
-window.blit(radiyuG[0], (x, y))
+# Import graphics
+radiyubase = pygame.image.load("images/base/0.png")
+radiyulowC = [pygame.image.load("images/1/0.png"), pygame.image.load("images/1/1.png"),
+              pygame.image.load("images/1/2.png")]
+radiyulowD = [pygame.image.load("images/2/0.png"), pygame.image.load("images/2/1.png"),
+              pygame.image.load("images/2/2.png")]
+radiyuE = [pygame.image.load("images/3/0.png"), pygame.image.load("images/3/1.png"),
+           pygame.image.load("images/3/2.png")]
+radiyuF = [pygame.image.load("images/4/0.png"), pygame.image.load("images/4/1.png")]
+radiyuG = [pygame.image.load("images/5/0.png"), pygame.image.load("images/5/1.png")]
+radiyuA = [pygame.image.load("images/6/0.png"), pygame.image.load("images/6/1.png"),
+           pygame.image.load("images/6/2.png")]
+radiyuB = [pygame.image.load("images/7/0.png"), pygame.image.load("images/7/1.png")]
+radiyuhighC = [pygame.image.load("images/8/0.png"), pygame.image.load("images/8/1.png"),
+               pygame.image.load("images/8/2.png")]
+radiyuhighD = [pygame.image.load("images/9/0.png")]
+# radiyuSpecial = #gif_link
+window.blit(radiyubase, (0, 0))
+
 
 # 3 define functions for each note
 def playA():
-    window.blit(radiyuA[1], (x, y))
-    pygame.mixer.sound.play(A, loops = -1)
-    window.blit(radiyuA[2], (x, y))
+    window.blit(radiyuA[1], (0, 0))
+    pygame.mixer.Sound.play(A, loops=-1)
+    window.blit(radiyuA[2], (0, 0))
+
+
 def playB():
-    window.blit(radiyuB[1], (x, y))
-    pygame.mixer.sound.play(B, loops = -1)
-    window.blit(radiyuB[2], (x, y))
-def playC():
-    window.blit(radiyuC[1], (x, y))
-    pygame.mixer.sound.play(C, loops = -1)
-    window.blit(radiyuC[2], (x, y))
-def playD():
-    window.blit(radiyuD[1], (x, y))
-    pygame.mixer.sound.play(D, loops = -1)
-    window.blit(radiyuD[2], (x, y))
+    window.blit(radiyuB[1], (0, 0))
+    pygame.mixer.Sound.play(B, loops=-1)
+    window.blit(radiyuB[2], (0, 0))
+
+
+def playlowC():
+    window.blit(radiyulowC[1], (0, 0))
+    pygame.mixer.Sound.play(lowC, loops=-1)
+    window.blit(radiyulowC[2], (0, 0))
+
+
+def playlowD():
+    window.blit(radiyulowD[1], (0, 0))
+    pygame.mixer.Sound.play(lowD, loops=-1)
+    window.blit(radiyulowD[2], (0, 0))
+
+
 def playE():
-    window.blit(radiyuE[1], (x, y))
-    pygame.mixer.sound.play(E, loops = -1)
-    window.blit(radiyuE[2], (x, y))
+    window.blit(radiyuE[1], (0, 0))
+    pygame.mixer.Sound.play(E, loops=-1)
+    window.blit(radiyuE[2], (0, 0))
+
+
 def playF():
-    window.blit(radiyuF[1], (x, y))
-    pygame.mixer.sound.play(F, loops = -1)
-    window.blit(radiyuF[2], (x, y))
+    window.blit(radiyuF[1], (0, 0))
+    pygame.mixer.Sound.play(F, loops=-1)
+    window.blit(radiyuF[2], (0, 0))
+
+
 def playG():
-    window.blit(radiyuG[1], (x, y))
-    pygame.mixer.sound.play(G, loops = -1)
-    window.blit(radiyuG[2], (x, y))
+    window.blit(radiyuG[1], (0, 0))
+    pygame.mixer.Sound.play(G, loops=-1)
+    window.blit(radiyuG[2], (0, 0))
+
+
+def playhighC():
+    window.blit(radiyuhighC[1], (0, 0))
+    pygame.mixer.Sound.play(highC, loops=-1)
+    window.blit(radiyuhighC[2], (0, 0))
+
+
+def playhighD():
+    window.blit(radiyuhighD[1], (0, 0))
+    pygame.mixer.Sound.play(highD, loops=-1)
+    window.blit(radiyuhighD[2], (0, 0))
+
+
+def playSpecial():
+    pygame.mixer.Sound.play(Special, loops=-1)
+
 
 # 4 always loop
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type == pygame.KEYDOWN:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_1]:
-                processes.append(Process(target = playC))
-            if keys[pygame.K_2]:
-                processes.append(Process(target = playD))
-            if keys[pygame.K_3]:
-                processes.append(Process(target = playE))
-            if keys[pygame.K_4]:
-                processes.append(Process(target = playF))
-            if keys[pygame.K_5]:
-                processes.append(Process(target = playG))
-            if keys[pygame.K_6]:
-                processes.append(Process(target = playA))
-            if keys[pygame.K_7]:
-                processes.append(Process(target = playB))
+if __name__ == "__main__":
+    processes = []
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_1]:
+                    processes.append(Process(target=playlowC))
+                if keys[pygame.K_2]:
+                    processes.append(Process(target=playlowD))
+                if keys[pygame.K_3]:
+                    processes.append(Process(target=playE))
+                if keys[pygame.K_4]:
+                    processes.append(Process(target=playF))
+                if keys[pygame.K_5]:
+                    processes.append(Process(target=playG))
+                if keys[pygame.K_6]:
+                    processes.append(Process(target=playA))
+                if keys[pygame.K_7]:
+                    processes.append(Process(target=playB))
+                if keys[pygame.K_8]:
+                    processes.append(Process(target=playhighC))
+                if keys[pygame.K_9]:
+                    processes.append(Process(target=playhighD))
 
-            for process in processes:
-                process.start()
-            for process in processes:
-                process.join()
+                for process in processes:
+                    process.start()
+                for process in processes:
+                    process.join()
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_1:
-                window.blit(radiyuC[1], (x, y))
-                pygame.mixer.sound.stop(C)
-                window.blit(radiyuC[0], (x, y))
-            if event.key == pygame.K_2:
-                window.blit(radiyuD[1], (x, y))
-                pygame.mixer.sound.stop(D)
-                window.blit(radiyuD[0], (x, y))
-            if event.key == pygame.K_3:
-                window.blit(radiyuE[1], (x, y))
-                pygame.mixer.sound.stop(E)
-                window.blit(radiyuE[0], (x, y))
-            if event.key == pygame.K_4:
-                window.blit(radiyuF[1], (x, y))
-                pygame.mixer.sound.stop(F)
-                window.blit(radiyuF[0], (x, y))
-            if event.key == pygame.K_5:
-                window.blit(radiyuG[1], (x, y))
-                pygame.mixer.sound.stop(G)
-                window.blit(radiyuG[0], (x, y))
-            if event.key == pygame.K_6:
-                window.blit(radiyuA[1], (x, y))
-                pygame.mixer.sound.stop(A)
-                window.blit(radiyuA[0], (x, y))
-            if event.key == pygame.K_7:
-                window.blit(radiyuB[1], (x, y))
-                pygame.mixer.sound.stop(B)
-                window.blit(radiyuB[0], (x, y))
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_1:
+                    window.blit(radiyulowC[2], (0, 0))
+                    pygame.mixer.Sound.stop(lowC)
+                    window.blit(radiyubase, (0, 0))
+                if event.key == pygame.K_2:
+                    window.blit(radiyulowD[2], (0, 0))
+                    pygame.mixer.Sound.stop(lowD)
+                    window.blit(radiyubase, (0, 0))
+                if event.key == pygame.K_3:
+                    window.blit(radiyuE[2], (0, 0))
+                    pygame.mixer.Sound.stop(E)
+                    window.blit(radiyubase, (0, 0))
+                if event.key == pygame.K_4:
+                    window.blit(radiyuF[0], (0, 0))
+                    pygame.mixer.Sound.stop(F)
+                    window.blit(radiyubase, (0, 0))
+                if event.key == pygame.K_5:
+                    window.blit(radiyuG[0], (0, 0))
+                    pygame.mixer.Sound.stop(G)
+                    window.blit(radiyubase, (0, 0))
+                if event.key == pygame.K_6:
+                    window.blit(radiyuA[2], (0, 0))
+                    pygame.mixer.Sound.stop(A)
+                    window.blit(radiyubase, (0, 0))
+                if event.key == pygame.K_7:
+                    window.blit(radiyuB[0], (0, 0))
+                    pygame.mixer.Sound.stop(B)
+                    window.blit(radiyubase, (0, 0))
+                if event.key == pygame.K_8:
+                    window.blit(radiyuhighC[2], (0, 0))
+                    pygame.mixer.Sound.stop(highC)
+                    window.blit(radiyubase, (0, 0))
+                if event.key == pygame.K_9:
+                    pygame.mixer.Sound.stop(highD)
+                    window.blit(radiyubase, (0, 0))
 
-    pygame.display.update()
-
-
+        pygame.display.update()
